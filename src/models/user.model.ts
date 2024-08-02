ed0617a5dcc428548febd5f73e292c4f3e540df5
yasg-uru <yashpawar12122004@gmail.com>
2024-08-02 14:06:56 +0530

    created usermodel

---
+++ src/models/user.model.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, Document, mongo } from "mongoose";
+export interface User extends Document {
+  username: string;
+  email: string;
+  passwordHash: string;
+  oauth2: {
+    provider: string;
+    providerId: string;
+    token: string;
+    refreshedToken: string;
+  };
+  createdAt: Date;
+  updatedAt: Date;
+}
+const UserSchema: Schema<User> = new Schema<User>(
+  {
+    username: {
+      type: String,
+      required: [true, "user name is required"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "email is required"],
+    },
+    passwordHash: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    oauth2: {
+      provider: {
+        type: String,
+      },
+      providerId: String,
+      token: String,
+      refreshedToken: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const userModel = mongoose.model<User>("User", UserSchema);
+export default userModel;
