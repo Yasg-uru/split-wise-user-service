12799919539cd6e0099d5b9b0859b0c7e649c419
yasg-uru <yashpawar12122004@gmail.com>
2024-08-03 13:38:20 +0530

    setuped the server

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,18 @@
-import express from "express";
-import dotenv from "dotenv";
-import session from "express-session";
-import passport from "passport";
-import connectDatabase from "./utils/connectDatabase";
-import authRouter from "./routes/auth.route";
-import "./config/passport-setup";
-import { errorMiddleware } from "./utils/Errorhandler.util";
-import cookieParser from "cookie-parser";
+
+import express, { Express, Request, Response , Application } from 'express';
+import dotenv from 'dotenv';
+
+//For env File 
 dotenv.config();
 
-const app = express();
+const app: Application = express();
+const port = process.env.PORT || 8000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET as string,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24,
-    },
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Use the auth routes
-app.use("/auth", authRouter);
-
-// Connect to the database
-connectDatabase();
-app.use(errorMiddleware);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server is running on port:", PORT);
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server');
 });
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
+
